f9e25dedadd2c3b942d23c8c14e67a6f03f0480d
Jose A. <informaticsproservices@gmail.com>
2025-09-26 17:59:05 -0500

    edn point prueba2

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/api/test-email", async (req, res) => {
   try {
     await transporter.sendMail({
       from: `"CESI 2025" <${process.env.EMAIL_USER}>`,
-      to: "tu_correo_prueba@gmail.com", // cámbialo por un correo real tuyo
+      to: "jose.acosta@unachi.ac.pa", // cámbialo por un correo real tuyo
       subject: "Prueba Render SMTP",
       text: "¡Funciona el envío desde Render con Gmail!"
     });
